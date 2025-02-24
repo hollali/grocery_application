ebcdec6ffdb0c36dc9c5eb09362a281259aae210
hollali <dheztinykartel@gmail.com>
2025-02-24 13:03:31 +0000

    first commit

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen  extends StatelessWidget{
+  const SplashScreen({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: [
+          //* App Logo
+          Padding(padding: const EdgeInsets.only(
+            left: 80.0,bottom: 40.0,top: 160,
+          ),
+          child: Image.asset('lib/assets/logo/logo - 1.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}

4019287048b5a86be2fff5ef745ff98262e69d86
hollali <dheztinykartel@gmail.com>
2025-02-25 10:55:50 +0000

    Refactor SplashScreen layout for improved padding consistency

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen  extends StatelessWidget{
+class SplashScreen extends StatelessWidget {
   const SplashScreen({required Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           //* App Logo
-          Padding(padding: const EdgeInsets.only(
-            left: 80.0,bottom: 40.0,top: 160,
-          ),
-          child: Image.asset('lib/assets/logo/logo - 1.png'),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 80.0,
+              right: 80,
+              bottom: 40.0,
+              top: 160,
+            ),
+            child: Image.asset('lib/assets/logo/logo - 1.png'),
           ),
         ],
       ),

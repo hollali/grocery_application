6a6eced7da9b2e9b15beab46546ef7cfced576ef
hollali <dheztinykartel@gmail.com>
2025-02-26 19:46:31 +0000

    Add MyTextField component for reusable text input functionality

---
+++ lib/assets/components/my_textfield.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  final TextStyle? textStyle;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    this.textStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        style: textStyle,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            borderSide: BorderSide(color: Colors.grey)
+          ),
+        ),
+      ),
+    );
+  }
+}
+

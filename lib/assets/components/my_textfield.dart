f62041eab006736296fc3bf66dd8ba0f373fd3ed
hollali <dheztinykartel@gmail.com>
2025-03-03 12:19:52 +0000

    Refactor MyTextField to StatefulWidget and add prefixIcon and suffixIcon functionality; implement login page with username input field.

--- lib/assets/components/my_textfield.dart
+++ lib/assets/components/my_textfield.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget {
+class MyTextField extends StatefulWidget {
   final controller;
   final String hintText;
   final bool obscureText;
   final TextStyle? textStyle;
+  final IconData? prefixIcon;
 
   const MyTextField({
     super.key,
@@ -12,24 +13,58 @@ class MyTextField extends StatelessWidget {
     required this.hintText,
     required this.obscureText,
     this.textStyle,
+    this.prefixIcon,
   });
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  late bool obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    obscureText = widget.obscureText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
-        controller: controller,
+        controller: widget.controller,
         obscureText: obscureText,
-        style: textStyle,
+        style: widget.textStyle,
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            borderSide: BorderSide(color: Colors.grey)
-          ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              borderSide: BorderSide(color: Colors.grey)),
+
+              focusedBorder:  OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
         ),
+        fillColor: Colors.white,
+        filled: true,
+        hintText: widget.hintText,
+        hintStyle: const TextStyle(color: Colors.grey),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        prefixIcon: widget.prefixIcon != null
+        ? Icon(widget.prefixIcon)
+        : null,
+        suffixIcon: widget.obscureText 
+        ? IconButton(
+          onPressed: () {
+            setState(() {
+              obscureText = !obscureText;
+            });
+          },
+          icon: Icon(obscureText ? Icons.visibility : Icons.visibility_off),
+        )
+        : null,
+      ),
       ),
     );
   }
 }
-
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
+import '../assets/components/my_textfield.dart';
+
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
+  //* Text editing controllers
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  //* Login Function
+  void login() {}
+
+  //* Build Method
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: SingleChildScrollView(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              //* Logo
+              Image.asset(
+                'assets/images/logo.png',
+                width: 100,
+              ),
+              const SizedBox(height: 50),
+
+              //* Welcome Text
+              Text(
+                'Welcome Back!',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[700],
+                  fontFamily: 'Poppins',
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              //* Username TextField
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+                textStyle: TextStyle(fontFamily: 'Poppins'),
+                prefixIcon: Icons.person,
+              ),
+            ],
+          )),
+        ),
+      )),
+    );
   }
 }

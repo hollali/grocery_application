2fd9818d57c41eac15992ed071d9070dac9d4326
hollali <dheztinykartel@gmail.com>
2025-05-06 12:11:55 +0000

    Enhance user experience in SignUpPage by adding loading indicators during account creation process

---
+++ lib/pages/authentication.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:grocery_application/pages/dashboard_page.dart';
+import 'package:grocery_application/pages/login_page.dart';
+
+class AuthenticationPage extends StatelessWidget {
+  const AuthenticationPage({super.key});
+
+  State<AuthenticationPage> createState()
+}

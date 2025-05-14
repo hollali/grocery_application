fc6476d5f508c225f230a38d6032f20c0df15e70
hollali <dheztinykartel@gmail.com>
2025-05-14 11:07:29 +0000

    Add Firebase initialization imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());

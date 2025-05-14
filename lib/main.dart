bcabe1b7ba7ae90d0dcac7c7a2d74fc13ef46630
hollali <dheztinykartel@gmail.com>
2025-05-14 11:55:03 +0000

    Initialize Firebase in main function and ensure Flutter bindings are ready

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'pages/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
